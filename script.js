8111706dee154e6cf8ea557e4e90879f4038ef99
Vicent Botella Ferragud <vicentbotella.93@gmail.com>
2022-08-08 13:30:13 +0200

    shopping cart finished

--- script.js
+++ script.js
@@ -6,87 +6,152 @@ class ShoppingCart {
 
 
     addToCart(product, price, quantity) {
+
+        let emptyCart = document.getElementById('emptyCart');
+        emptyCart.classList.add('d-none');
+
         let index = this.products.indexOf(product);
+
         if (index === -1) {
+
             this.products.push(product);
             this.prices.push(price);
             this.quantities.push(quantity);
+
         } else {
+
             this.quantities[index]++;
+
         }
-        console.log(this.products, this.prices, this.quantities);
+        
         this.displayShoppingCart();
+
     }
 
     removeFromCart(product) {
+
         let index = this.products.indexOf(product);
+
         if (index !== -1) {
+
             this.products.splice(index, 1);
             this.prices.splice(index, 1);
             this.quantities.splice(index, 1);
+
         } else {
+
             return;
+
         }
-        console.log(this.products, this.prices, this.quantities);
+        
         this.displayShoppingCart();
+
     }
 
     decreaseQuantityByOne(product) {
+
         let index = this.products.indexOf(product);
+
         if (index !== -1) {
+
             this.quantities[index]--;
+
             if (this.quantities[index] === 0) {
+
                 this.removeFromCart(product);
+
             }
+
         } else {
+
             return;
+
         }
-        console.log(this.products, this.prices, this.quantities);
+        
         this.displayShoppingCart();
+
     }
 
     calculateTotal() {
+
         let total = 0;
+
         for (let i = 0; i < this.prices.length; i++) {
+
             let price = this.prices[i];
             let quantity = this.quantities[i];
-            total +=  price * quantity;
+            total += price * quantity;
+
         }
-        return total;
+
+        return total.toFixed(2);
+
     }
 
     countProducts() {
+
         let count = 0;
+
         for (let i = 0; i < this.quantities.length; i++) {
+
             let quantity = this.quantities[i];
-            count +=  quantity;
+            count += quantity;
+
         }
+
         return count;
+
     }
 
     displayShoppingCart() {
+
         let addedProductsSection = document.getElementById('addedProductsSection');
         addedProductsSection.innerHTML = '';
+
         for (let i = 0; i < this.products.length; i++) {
+
             let product = this.products[i];
             let price = this.prices[i];
             let quantity = this.quantities[i];
+
             addedProductsSection.innerHTML += `
-                <div>
-                    <span>${product}</span>
-                    <span>${price}</span>
+
+                <div class="my-4 font-reduced-mobile">
+
+                    <span>${product} - </span>
+                    <span>${(price * quantity).toFixed(2)}€ x </span>
                     <span>${quantity}</span>
+                    <button class="plus-btn" onclick="shoppingCart.addToCart('${product}', ${price}, 1);">+</button>
+                    <button class="minus-btn" onclick="shoppingCart.decreaseQuantityByOne('${product}');">-</button>
+                    <button class="remove-btn" onclick="shoppingCart.removeFromCart('${product}');">Remove</button>
+
                 </div>
+
             `;
+
         }
+
         let totalPurchaseSection = document.getElementById('totalPurchaseSection');
         totalPurchaseSection.innerHTML = '';
-        totalPurchaseSection.innerHTML = `
-                <div>
-                    <span>${this.calculateTotal()}</span>
-                    <span>${this.countProducts()}</span>
-                </div>
+
+        if (this.countProducts() === 0) {
+
+            let emptyCart = document.getElementById('emptyCart');
+            emptyCart.classList.remove('d-none');
+
+            totalPurchaseSection.innerHTML = '';
+
+        } else {
+
+            totalPurchaseSection.innerHTML = `
+                
+                <span>Total amount of items: <b>${this.countProducts()}</b></span>
+                <span>Total price: <b>${this.calculateTotal()}€</b></span>
+                
             `;
+
+        }
+        
     }
 
 }
